2e92981f9799c0705639fae173073de3da89de0f
WangKang <888666wang4286@gmail.com>
2024-11-08 13:29:53 +0800

    修改内容

---
+++ src/views/search.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <input v-model="searchQuery" placeholder="Enter search terms...">
+      <ul>
+        <li v-for="card in filteredCards" :key="card.title">
+          <h3>{{ card.title }}</h3>
+          <p>{{ card.describe }}</p>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import { computed, ref } from 'vue';
+  
+  export default {
+    setup() {
+      const searchQuery = ref('');
+  
+      const filteredCards = computed(() => {
+        return cards.filter(card => card.describe.toLowerCase().includes(searchQuery.value.toLowerCase()));
+      });
+  
+      return {
+        searchQuery,
+        filteredCards
+      };
+    }
+  }
+  </script>
+  
